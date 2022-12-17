d2bd1af5570e39a6c14ff7ab5685dc62458881e5
DADAKIM <d.ya96v@gmail.com>
2022-12-17 21:09:30 +0900

    :wrench: [feat] 입력 폼 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
 
 function App() {
   const expenses = [
@@ -26,6 +28,7 @@ function App() {
   return (
     <div>
       <h2>hello world</h2>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,15 +1,25 @@
+import { useState } from 'react';
+
 import Card from '../Card/Card';
 import ExpenseDate from '../ExpenseDate/ExpenseDate'
 import './expenseItem.css'
 
 export default function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  function handleClick(){
+    setTitle('hi!');
+    console.log(title);
+  }
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item-desc'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item-price'>${props.amount}</div>
       </div>
+      <button onClick={handleClick}>Change Title</button>
     </Card>
   );
 }
