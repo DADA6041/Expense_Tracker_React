ffb15413e6182dcf044b3661b3f18533e460f28d
DADAKIM <d.ya96v@gmail.com>
2022-12-25 00:58:59 +0900

    :wrench: 필터 state 적용

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import Card from "../Card/Card";
+import ExpenseFilter from "../ExpenseFilter/ExpenseFilter";
 import ExpenseItem from "../ExpenseItem/ExpenseItem";
 import "./expenses.css";
 
 export default function Expenses({ expenses }) {
+  const [yearSelected, setYearSelected] = useState("2020");
+
+  const handleFilterChange = (selecedYear) => {
+    setYearSelected(selecedYear);
+    console.log(selecedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpenseFilter
+        yearSelected={yearSelected}
+        setYearSelect={setYearSelected}
+        onChangeFilter={handleFilterChange} />
       <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
       <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
       <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
